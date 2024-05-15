3427eaa32a5a098a7006c0a3693c0c7443884621
alonso092 <alonsoprofesional092@gmail.com>
2024-05-14 21:15:36 -0600

    script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 // Espera 5 segundos antes de redireccionar
 setTimeout(function() {
-    window.location.href = "../principal/principal.html";
+    window.location.href = "/principal/principal.html";
 }, 5000);
